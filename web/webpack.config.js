6f84716d3066fd77243378dd18296fb09e1915c8
goujianjun <xyclr2010@gmail.com>
2015-11-20 00:42:16 +0800

    official website

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -31,6 +31,12 @@ module.exports = {
             {test: /\.scss$/, loader: "style!css!sass"},
             {test: /\.less/,loader: 'style-loader!css-loader!less-loader'},
             {test: /\.(jpg|png)$/, loader: "url-loader?limit=8192&name=/i/[hash:8].[name].[ext]"},
+            {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+            {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff2"},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
+
         ]
     },
 
